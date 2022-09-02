aaa4eb34a61a745c6ede8b70f996231f1fcbde93
FelipeAngeloziSG <felipe.angelozi@sgsistemas.com.br>
2022-09-02 10:05:37 -0300

    Testando Merge no git

--- hello-git/src/principal/Programa.java
+++ hello-git/src/principal/Programa.java
@@ -5,5 +5,12 @@ public class Programa {
     public static void main(String[] args) {
 
         System.out.println("Hello GIT!");
+
+        imprimir();
+
+    }
+
+    public static void imprimir(){
+        System.out.println("teste do metodo imprimir");
     }
 }
