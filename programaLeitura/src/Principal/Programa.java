7e020fd9be15dd5cd1e329fb70c0aa480cbce0e4
FelipeAngeloziSG <felipe.angelozi@sgsistemas.com.br>
2022-09-09 08:02:03 -0300

    Programa de leitura de tabelas

--- programaLeitura/src/Principal/Programa.java
+++ programaLeitura/src/Principal/Programa.java
@@ -1,2 +1,72 @@
-package Principal;public class Programa {
+// melhorar classes e rebaixar os txts novamente
+
+package Principal;
+
+
+import sun.nio.fs.GnomeFileTypeDetector;
+
+import java.util.Scanner;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Tabelas tabelas = new Tabelas();
+        String nomeTabela = "";
+
+        while (true) {
+
+            System.out.println("--- Qual tabela deseja visualizar ---\n" +
+                    "[1] - Tabela_4_3_10\n" +
+                    "[2] - Tabela_4_3_12\n" +
+                    "[3] - Tabela_4_3_13\n" +
+                    "[4] - Tabela_4_3_14\n" +
+                    "[5] - Tabela_4_3_15\n" +
+                    "[6] - Tabela_4_3_15\n" +
+                    "[0] - Sair\n");
+
+            int respostaMenu = 0;
+            respostaMenu = sc.nextInt();
+
+            switch (respostaMenu) {
+                case 1: {
+                    nomeTabela = "Tabela_4_3_10.txt";
+                    break;
+                }
+                case 2: {
+                    nomeTabela = "Tabela_4_3_12.txt";
+                    break;
+                }
+                case 3: {
+                    nomeTabela = "Tabela_4_3_13.txt";
+                    break;
+                }
+                case 4: {
+                    nomeTabela = "Tabela_4_3_14.txt";
+                    break;
+                }
+                case 5: {
+                    nomeTabela = "Tabela_4_3_15.txt";
+                    break;
+                }
+                case 6: {
+                    nomeTabela = "Tabela_4_3_16.txt";
+                    break;
+                }
+                default: {
+                    break;
+                }
+
+            }
+
+            if (respostaMenu == 0) {
+                break;
+            }
+
+            tabelas.popularLista(nomeTabela);
+            tabelas.imprimirLista();
+        }
+
+
+    }
 }
--- programaLeitura/src/Principal/Tabelas.java
+++ programaLeitura/src/Principal/Tabelas.java
@@ -6,44 +6,40 @@ import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Tabela1 {
+public class Tabelas {
 
-    ArrayList<String> listaTabela1 = new ArrayList<String>();
+    ArrayList<String> listaTabela = new ArrayList<String>();
 
-    public Tabela1() {
+    public void popularLista(String tabela) {
         try {
 
-            Scanner scanner = new Scanner(new File("Tabela_4_3_10.txt"), "windows-1252");
+            Scanner scanner = new Scanner(new File(tabela), "windows-1252");
 
             while (scanner.hasNextLine()) {
                 String linha = scanner.nextLine();
                 Scanner linhaScanner = new Scanner(linha);
-
-
                 linhaScanner.useDelimiter("\\|");
-
-
-                Integer codigo = linhaScanner.nextInt();
+                String codigo = linhaScanner.next();
                 String descricao = linhaScanner.next();
                 String DTinicio = linhaScanner.next();
                 String DTfim = linhaScanner.next();
                 String NCM = linhaScanner.next();
                 String NCM_EX = linhaScanner.next();
                 String EX_IPI = linhaScanner.next();
-                double aliq_pis = linhaScanner.nextDouble();
-                double aliq_cofin = linhaScanner.nextDouble();
+                String aliq_pis = linhaScanner.next();
+                String aliq_cofin = linhaScanner.next();
 
-                String itemLista = String.format(Locale.GERMAN, "Cód           : %04d%n" +
+                String itemLista = String.format(Locale.GERMAN, "Cód           : %s%n" +
                         "Descrição     : %s%n" +
                         "DTInício      : %S%n" +
                         "DTFim         : %s%n" +
                         "NCM           : %s%n" +
                         "NCM_EX        : %s%n" +
                         "EX_IPI        : %s%n" +
-                        "Alíquota PIS  : %.2f%n" +
-                        "Alíquota Cofin: %.2f%n", codigo, descricao, DTinicio, DTfim, NCM , NCM_EX, EX_IPI, aliq_pis , aliq_cofin);
+                        "Alíquota PIS  : %s%n" +
+                        "Alíquota Cofin: %s%n", codigo, descricao, DTinicio, DTfim, NCM , NCM_EX, EX_IPI, aliq_pis , aliq_cofin);
 
-                this.listaTabela1.add(itemLista);
+                this.listaTabela.add(itemLista);
             }
 
         } catch (FileNotFoundException exception) {
@@ -52,7 +48,7 @@ public class Tabela1 {
     }
 
     public void imprimirLista() {
-        this.listaTabela1.forEach(String -> System.out.println(String));
+        this.listaTabela.forEach(String -> System.out.println(String));
     }
 }
 
