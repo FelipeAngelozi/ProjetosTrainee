819d7a6bdb78db3c8a9224674a92daae8f2f50ac
FelipeAngeloziSG <felipe.angelozi@sgsistemas.com.br>
2022-09-19 18:00:36 -0300

    Inicio do programa  para teste de cardinalidade em JPA

---
+++ loja/src/main/java/br/com/sgsistemas/trainee/loja/util/JPAUtil.java
@@ -0,0 +1,2 @@
+package br.com.sgsistemas.trainee.loja.util;public class JPAUtil {
+}
---
+++ sgERPDB/src/main/java/br/com/sgsistemas/trainee/sgERPDB/model/Cidade.java
@@ -0,0 +1,10 @@
+package br.com.sgsistemas.trainee.sgERPDB.model;
+
+public class Cidade {
+
+
+    private String codigo;
+    private String nome;
+    private UF uf;
+
+}
